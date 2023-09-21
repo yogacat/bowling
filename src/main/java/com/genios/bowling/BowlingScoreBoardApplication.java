70a3d0c8c8d77275dd35c7a9a356db768561e921
oopenko <olena.openko@jambit.com>
2023-09-21 21:14:56 +0200

    Initial structure

---
+++ src/main/java/com/genios/bowling/BowlingScoreBoardApplication.java
@@ -0,0 +1,13 @@
+package com.genios.bowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingScoreBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BowlingScoreBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genios/bowling/BowlingScoreBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genios.bowling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingScoreBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
