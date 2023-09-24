32a92a22e30f9b2f290c06002311c0b70482e1ff
oopenko <olena.openko@jambit.com>
2023-09-24 13:28:06 +0200

    Added the first service

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -69,6 +69,9 @@ document.addEventListener('DOMContentLoaded', function () {
         // Game is over, hide the submit roll form
         submitRollForm.style.display = 'none';
         clearInterval(refreshInterval); // Stop refreshing statistics
+
+        // Fetch and display final statistics
+        displayPlayerScoreStatistics(playerId);
       }
     })
     .catch(error => {
